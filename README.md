fb690470c5446527543fd1b2f1efd6d1df4ae5fc
leeink <zzxc78211@gmail.com>
2024-10-01 14:49:26 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# MTVS Academy
+
+Final Project
+
+Developed with Unreal Engine 5
---
+++ Source/Maverick/Maverick.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Maverick.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Maverick, "Maverick" );
