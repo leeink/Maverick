3ad8ed1b14d18be02d079d937db9dff0f4707255
Jun0123 <76517458+Jun0123@users.noreply.github.com>
2024-10-04 13:18:50 +0900

    TopDown컨텐츠 추가, AI 캐릭터 이동 중

---
+++ Source/Maverick/Public/XR_LSJ/AISquadFSMComponent.h
@@ -0,0 +1,66 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "AISquadFSMComponent.generated.h"
+
+UENUM(BlueprintType)
+enum class EEnemyState : uint8
+{
+	IDLE UMETA(DisplayName = "대기") ,
+	MOVE  UMETA(DisplayName = "이동") ,
+	ATTACK  UMETA(DisplayName = "공격") ,
+	DAMAGE UMETA(DisplayName = "데미지") ,
+	DIE UMETA(DisplayName = "죽음") ,
+};
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class MAVERICK_API UAISquadFSMComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UAISquadFSMComponent();
+	EEnemyState State = EEnemyState::IDLE;
+	void SetState(EEnemyState NextState);
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+	void TickIdle(const float& DeltaTime);
+	void TickMove(const float& DeltaTime);
+	void TickAttack(const float& DeltaTime);
+	void TickDamage(const float& DeltaTime);
+	void TickDie(const float& DeltaTime);
+
+	void MoveToArrivalPoint();
+	void MoveToTarget();
+	UPROPERTY()
+	class AAISquad* AISquadBody;
+	//이동 위치
+	UPROPERTY()
+	FVector ArrivalPoint=FVector::ZeroVector;
+	//공격 타겟
+	UPROPERTY()
+	class ACharacter* Target;
+	//공격 사정거리
+	float AttackDistance = 100.0f;
+	// 네비게이션을 이용해서 길찾기를 하고싶다.
+	UPROPERTY()
+	class AAIController* AISquadController;
+
+	FVector PatrolPoint;
+	float PatrolPointRadius = 500;
+	// 내위치에서 반경 5미터
+	bool SetPatrolPoint(FVector origin, float radius, FVector& dest);
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+		
+public:
+	FVector GetArrivalPoint() const { return ArrivalPoint; }
+	void SetArrivalPoint(FVector val) { ArrivalPoint = val; }
+};
