ba486e1b8faf024263e5f7a3347e8ed0f08cabd2
leeink <zzxc78211@gmail.com>
2024-10-08 15:12:45 +0900

    Add AI Base

---
+++ Source/Maverick/Public/LDG/RifleSoldier.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LDG/Soldier.h"
+#include "RifleSoldier.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAVERICK_API ARifleSoldier : public ASoldier
+{
+	GENERATED_BODY()
+	
+public:
+	ARifleSoldier();
+	
+public:
+	virtual void Wait() override;
+	virtual void Move() override;
+	virtual void Attack() override;
+};
---
+++ Source/Maverick/Public/LDG/Soldier.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Soldier.generated.h"
+
+UCLASS()
+class MAVERICK_API ASoldier : public ACharacter
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Stat, meta = (AllowPrivateAccess = "true"))
+	float Speed;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Stat, meta = (AllowPrivateAccess = "true"))
+	float AttackPower;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Stat, meta = (AllowPrivateAccess = "true"))
+	float AttackRate;
+
+public:
+	// Sets default values for this character's properties
+	ASoldier();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void Wait();
+	virtual void Move();
+	virtual void Attack();
+};
