acb11cc06fef3c8d3957ba104c0e5eebacef078a
Jun0123 <76517458+Jun0123@users.noreply.github.com>
2024-11-25 18:08:02 +0900

    패배 UI 애니메이션

--- Source/Maverick/Private/XR_LSJ/ResultIconWidget.cpp
+++ Source/Maverick/Private/XR_LSJ/ResultIconWidget.cpp
@@ -3,11 +3,15 @@
 
 #include "XR_LSJ/ResultIconWidget.h"
 #include "Components/TextBlock.h"
-
+#include "Animation/WidgetAnimation.h"
 void UResultIconWidget::SetVisibleIcon(bool Win)
 {
 	if(Win)
 		Text_Win->SetVisibility(ESlateVisibility::Visible);
 	else
+	{
+		PlayAnimation(Defeat);
 		Text_Defeat->SetVisibility(ESlateVisibility::Visible);
+	}
+		
 }
